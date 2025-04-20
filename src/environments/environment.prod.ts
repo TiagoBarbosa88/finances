92c0b95dd26c2f66ed12e823c50137b55f6ec4d5
GiselleBarbosa <g88barbosa@gmail.com>
2025-04-20 15:38:11 -0300

    wip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   transactionUrl:
-    'https://backend-finances-ms-transactions.onrender.com/api/transactions',
+    'https://backend-finances-ms-transactions.onrender.com/transactions',
   categoryUrl:
-    'https://backend-finances-ms-transactions.onrender.com/api/categories',
-  userUrl: 'https://backend-finances-ms-user.onrender.com/api/users',
+    'https://backend-finances-ms-transactions.onrender.com/categories',
+  userUrl: 'https://backend-finances-ms-user.onrender.com/users',
 };
