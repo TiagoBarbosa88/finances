84605db0287f7711200601482f3f5f564c594636
GiselleBarbosa <g88barbosa@gmail.com>
2025-04-20 15:17:28 -0300

    refac: altera paths em environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: false,
-  transactionUrl: '/api/transactions',
-  categoryUrl: '/api/categories',
-  userUrl: '/api/users'
+  transactionUrl:
+    'https://backend-finances-ms-transactions.onrender.com/transactions',
+  categoryUrl:
+    'https://backend-finances-ms-transactions.onrender.com/categories',
+  userUrl: 'https://backend-finances-ms-user.onrender.com/api/users',
 };
